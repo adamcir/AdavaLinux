a1445500abe5801c26b95a6fdc5d18ded234ac8e
adamcir <adam.cir@ptw.cz>
2026-09-20 16:34:35 +0200

    Test Fedora GRUB2 installer dependency path

--- tests/grub_installer_dependency_bundle_test.sh
+++ tests/grub_installer_dependency_bundle_test.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 set -eu
 
+installer=tools/installer/install.c
+helpers=tools/installer/helpers.c
 makefile=Makefile
 
-grep -Fq 'SYSPCKG_PACKAGE_DIR ?= $(PROJECT_DIR)/../syspckg/packages' "$makefile"
-grep -Fq 'grub-bios grub-efi brotli bzip2 freetype libpng xz zlib' "$makefile"
-grep -Fq 'Missing installer GRUB dependency package:' "$makefile"
-grep -Fq 'cp -f "$$pkg" "$$ROOTFS_DIR/usr/share/syspckg/packages/"' "$makefile"
+# GRUB comes from Fedora through the current SystemPackager.
+grep -Fq '"grub2-pc"' "$installer"
+grep -Fq '"grub2-efi-x64"' "$installer"
+grep -Fq '"grub2-efi-x64-modules"' "$installer"
+grep -Fq '"--source"' "$helpers"
+grep -Fq '"fedora"' "$helpers"
 
-printf '%s\n' 'GRUB installer dependency bundle test passed'
+if grep -Fq 'SYSPCKG_PACKAGE_DIR' "$makefile"; then
+  printf '%s\n' 'legacy local .syspckg GRUB bundle is still configured' >&2
+  exit 1
+fi
+
+printf '%s\n' 'Fedora GRUB2 installer dependency test passed'
